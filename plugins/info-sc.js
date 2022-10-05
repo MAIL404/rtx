ec3b96048cfbddb312a38e1044e40d83872f0e62
MAIL404 <113687471+MAIL404@users.noreply.github.com>
2022-10-05 22:57:19 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let ye = `@${m.sender.split`@`[0]}`
 let esce = `
-Hai ${ye} Bot Ini Menggunakan Script :\n• https://github.com/BOTCAHX/RTXZY-MD
+Hai ${ye} Bot Ini Menggunakan Script :\n•😂
 `
 conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
 }
